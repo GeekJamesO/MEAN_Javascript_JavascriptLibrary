8a20880ec02367f3194415099c1808c75eb559d6
JamesORourke <geekjamesorourkejr@icloud.com>
2017-11-02 15:03:24 -0700

    Done

---
+++ lib.js
@@ -0,0 +1,18 @@
+function Node(value) {
+    this.value = value;
+    this.next = null;
+    return this;
+}
+Node.prototype.push(this, value) {
+
+}
+
+function Sll() {
+    this.head = null;
+}
+
+Sll.prototype.add = function(node) {
+    if (this.head == null) {
+        this.head = node;
+    } else
+}
